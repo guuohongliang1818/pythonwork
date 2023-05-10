016f08e5983a94a249530c72d771f94d9fccd7c3
guohongliang <guohongliang@163.com>
2023-05-10 20:26:28 +0800

    yaml文件学习

---
+++ test_demo/test_yaml.py
@@ -0,0 +1,24 @@
+# 姓名：郭宏亮
+# 时间：2023/5/10 19:52
+import pytest
+import yaml
+
+from test_demo.utils import Utils
+
+
+def test_yaml1():
+    with open("./volume.yaml", "r", encoding="utf-8") as file:
+        # 将yaml文件的内容转成python数据结构
+        volume = yaml.safe_load(file)
+    print(volume)
+
+
+def test_yaml2():
+    with open("./data.yaml", "r", encoding="utf-8") as file:
+        data = yaml.safe_load(file)
+    print(data)
+
+
+@pytest.mark.parametrize("volume", Utils.load_yaml("./volume.yaml"))
+def test_yaml3(volume):
+    print(f"volume:{volume}")
---
+++ test_demo/utils.py
@@ -0,0 +1,26 @@
+# 姓名：郭宏亮
+# 时间：2023/5/10 20:07
+import yaml
+
+
+class Utils:
+    @classmethod
+    def load_yaml(cls, yaml_path):
+        with open(yaml_path, "r", encoding="utf-8") as file:
+            return yaml.safe_load(file)
+
+
+def reader(yaml_path):
+
+    with open(yaml_path, "r", encoding="utf-8") as file:
+        yaml_data = yaml.safe_load(file)
+
+    def inner():
+        pass
+
+    return inner
+
+
+if __name__ == '__main__':
+    data = Utils.load_yaml("./data.yaml")
+    print(data)
