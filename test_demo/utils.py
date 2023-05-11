4a11e3387493a963d58a57c3ffed545b0029e489
guohongliang <guohongliang@eyou.com>
2023-05-11 17:00:06 +0800

    装饰器，闭包函数

--- test_demo/utils.py
+++ test_demo/utils.py
@@ -10,17 +10,26 @@ class Utils:
             return yaml.safe_load(file)
 
 
-def reader(yaml_path):
-
-    with open(yaml_path, "r", encoding="utf-8") as file:
-        yaml_data = yaml.safe_load(file)
-
-    def inner():
-        pass
-
+def reader(func):
+    def inner(*args):
+        with open(args[0], "r", encoding="utf-8") as file:
+            yaml_data = yaml.safe_load(file)
+        # result = func(*args)
+        return yaml_data
     return inner
 
 
+@reader
+def read_yaml(yaml_path):
+    pass
+
+
+# inn = reader(read_yaml)
+
 if __name__ == '__main__':
-    data = Utils.load_yaml("../test_game/hero_info.yaml")
-    print(data)
+    # data = Utils.load_yaml("../test_game/hero_info.yaml")
+    # print(data)
+    lst = [1, 2, 3, 4, 5]
+    print(lst[0])
+    res = read_yaml("../test_game/test_create_hero_data.yaml")
+    print(res)
