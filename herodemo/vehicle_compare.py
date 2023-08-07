368db6cd94ae041254ffaa03c177f00ea4f6146e
guohongliang <guohongliang@eyou.com>
2023-08-07 09:32:40 +0800

    excel车辆比对bug修改

--- herodemo/vehicle_compare.py
+++ herodemo/vehicle_compare.py
@@ -6,7 +6,6 @@ list_1 = []
 excel = openpyxl.load_workbook("E:/wlhy-service/vehicle_type_info.xlsx")
 sheet = excel["vehicle_type_info"]
 print(sheet.values)
-# print(len(sheet.values))
 for i in sheet.values:
     if type(i[0]) is int:
         data = {}
@@ -14,7 +13,8 @@ for i in sheet.values:
         data["name"] = i[2]
         list_1.append(data)
 
-print(len(list_1))
+print("原始长度：", len(list_1))
+
 # print(list_1)
 
 list_2 = []
@@ -29,12 +29,30 @@ for i in sheet1.values:
         data["name"] = i[2]
         list_2.append(data)
 
-print(len(list_2))
-# print(list_2)
+print("原始长度：", len(list_2))
 
+list_same_code = []
+list_same_name = []
 i = 0
 for data1 in list_1:
     for data2 in list_2:
         if data1.get("code") == data2.get("code") and data1.get("name") == data2.get("name"):
             i = i + 1
             print(i, data1, data2)
+            list_same_code.append(data1.get("code"))
+            list_same_name.append(data1.get("name"))
+
+print(len(list_same_code))
+# print(list_same_code)
+
+for data1 in list_1:
+    if data1.get("code") in list_same_code and data1.get("name") in list_same_name:
+        pass
+    else:
+        print(data1)
+print("=============")
+for data2 in list_2:
+    if data2.get("code") in list_same_code and data2.get("name") in list_same_name:
+        pass
+    else:
+        print(data2)
